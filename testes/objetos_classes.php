7c2e60ef8b8c34671f5a1975adb938689b6fa780
Hilder Nunes <hildersn@gmail.com0>
2021-08-27 22:46:52 -0300

    Verificando nome das classes com get class

---
+++ testes/objetos_classes.php
@@ -0,0 +1,22 @@
+<?php
+
+use Funcionario as GlobalFuncionario;
+
+class Funcionario
+{
+    public $nome;
+    public $salario;
+    public $departamento;
+    
+}
+
+class Estagiario extends Funcionario
+{
+    public $bolsa;
+}
+
+$bru = new Funcionario;
+$elo = new Estagiario;
+
+print get_class( $bru ). '<br >';
+print get_class( $elo );
