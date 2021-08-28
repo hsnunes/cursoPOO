920d43a822e8a5375bab13ec8c62d5191a49d9a5
Hilder Nunes <hildersn@gmail.com0>
2021-08-27 22:50:37 -0300

    Descobrindo classe pai por nome da calsse filha

--- testes/objetos_classes.php
+++ testes/objetos_classes.php
@@ -21,4 +21,7 @@ $elo = new Estagiario;
 print get_class( $bru ). '<br >';
 print get_class( $elo ). '<br >';
 
+// chamado pela variável
 print get_parent_class( $elo ). '<br >';
+// chamado pelo nome da classe
+print get_parent_class( 'Estagiario' ). '<br >';
