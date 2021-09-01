7afbe69dd232a79fb234e8b2471a6cba2860d7c1
Hilder Nunes <hildersn@gmail.com0>
2021-09-01 18:00:16 -0300

    criando branch para sub tarefa nivel1

--- modulo03/dados/nivel1/pessoa_save_insert.php
+++ modulo03/dados/nivel1/pessoa_save_insert.php
@@ -6,7 +6,6 @@ $conn = mysqli_connect('127.0.0.1', 'root', 'root', 'cursopoo');
 
 $result = mysqli_query($conn, "SELECT max(id) as next FROM pessoa");
 $row = mysqli_fetch_assoc($result);
-
 $next = ($row['next'] + 1);
 
 $sql = "INSERT INTO pessoa (id, nome, endereco, bairro, telefone, email, id_cidade)
